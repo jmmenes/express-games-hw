5aaddbcdbe77c4d6d195b2dc9540ce25e2ad3e17
John Meneses <jmmenes90@gmail.com>
2021-05-25 11:49:10 -0400

    Commented out app listen 3000 .

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ app.use(express.json());
 app.use("/", indexRouter);
 app.use("/api/game", gameRouter);
 
-app.listen(3000, function () {
-  console.log(`Server is running on port 3000`);
-});
+// app.listen(3000, function () {
+//   console.log(`Server is running on port 3000`);
+// });
 
 module.exports = app;
